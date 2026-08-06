cdaf13283d1f5cc0f7ab2124e2dc8aee370d46ef
jpdavanso <jpdavanso@hotmail.com>
2026-08-05 22:25:52 -0300

    Add files via upload

---
+++ aula2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    float a, b, c;
+    float resultado;
+
+    a = 10;
+    b = 10;
+    c = 10;
+
+    resultado = (a + b + c) / 3;
+
+    printf("resultado %f", resultado);
+
+    return 0;
+}
